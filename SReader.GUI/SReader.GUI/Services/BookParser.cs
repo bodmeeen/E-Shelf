584a3e6d03f9224662e302a59216c9ee759a2e9a
bodmeeen <maks.demianp@gmail.com>
2026-07-10 17:27:05 +0300

    refactor: the method for opening a book has been changed from copy-based to stream-based

--- SReader.GUI/SReader.GUI/Services/BookParser.cs
+++ SReader.GUI/SReader.GUI/Services/BookParser.cs
@@ -1,6 +1,6 @@
 using System.IO;
-
-namespace SReader.Console;
+using System.Net.Http;
+using Avalonia;
 using VersOne.Epub;
 using System.Text.RegularExpressions;
 using System.Net;
@@ -8,72 +8,86 @@ using Fb2.Document;
 using System.Linq;
 using System.Text;
 
+namespace SReader.Console;
+
+
 public class BookParser
-{ 
-    public static string ReadAnyBook(string filepath)
 {
-    if (filepath.EndsWith(".txt"))
-        return ReadTxt(filepath);
-    else if (filepath.EndsWith(".epub"))
-        return ReadEpubBook(filepath);
-    else if  (filepath.EndsWith(".fb2"))
-        return ReadFb2Book(filepath);
-    else
+    public static string ReadAnyBook(Stream fileStream, string fileName)
     {
-        return "This format is not supported";
-    }
-}
-
-private static string ReadTxt(string filepath)
-{
-    string fileText = File.ReadAllText(filepath);
-    return fileText;
-}
-
-private static string ReadEpubBook(string filepath)
-{
-    EpubBook book = EpubReader.ReadBook(filepath);
-    System.Console.WriteLine($"Title: {book.Title},  Author: {book.Author}\n");
-
-    StringBuilder fullBookText = new StringBuilder();
-    
-    foreach (var chapter in book.ReadingOrder)
-    {
-        // вирізання всіх HTML тегів
-        string cleanText = Regex.Replace(chapter.Content, "<.*?>", string.Empty);
-        // перетворення веб-символів на звичайні пробіли та лапки
-        cleanText = WebUtility.HtmlDecode(cleanText).Trim();
-        //якщо після очищення це не порожня сторінка
-        if (!string.IsNullOrWhiteSpace(cleanText))
+        string lowerName = fileName.ToLower();
+        
+        if (fileName.EndsWith(".txt"))
+            return ReadTxt(fileStream);
+        else if (fileName.EndsWith(".epub"))
+            return ReadEpubBook(fileStream);
+        else if (fileName.EndsWith(".fb2") || lowerName.EndsWith(".fb2.zip"))
+            return ReadFb2Book(fileStream);
+        else
         {
-            fullBookText.AppendLine(cleanText); // додавання тексту цього розділу до загального
-            fullBookText.AppendLine(); // додавання відступів між рядками
+            return "This format is not supported";
         }
     }
-    return fullBookText.ToString();
-}
 
-private static string ReadFb2Book(string filepath)
-{
-    var fb2Document = new Fb2Document();
+    // private static string FileStreamReader(Stream fileStream, string fileName)
+    // {
+    //     using (StreamReader streamReader = new StreamReader(fileStream))
+    //     {
+    //         return streamReader.ReadToEnd();
+    //     }
+    // }
 
-    using (var stream = File.OpenRead(filepath))
+    private static string ReadTxt(Stream fileStream)
     {
-        fb2Document.Load(stream); // завантаження stream в контейнер
+        using (StreamReader streamReader = new StreamReader(fileStream))
+        {
+            return streamReader.ReadToEnd();
+        }
     }
 
-    if (fb2Document.Title != null)
+    private static string ReadEpubBook(Stream fileStream)
     {
-        // в fb2 заголовок може містити форматування, тому потрібно перетворити все в чистий рядок
-        var titleText = fb2Document.Title.ToString() ?? "The title is not specified";
+        EpubBook book = EpubReader.ReadBook(fileStream);
+        System.Console.WriteLine($"Title: {book.Title},  Author: {book.Author}\n");
+
+        StringBuilder fullBookText = new StringBuilder();
+
+        foreach (var chapter in book.ReadingOrder)
+        {
+            // вирізання всіх HTML тегів
+            string cleanText = Regex.Replace(chapter.Content, "<.*?>", string.Empty);
+            // перетворення веб-символів на звичайні пробіли та лапки
+            cleanText = WebUtility.HtmlDecode(cleanText).Trim();
+            //якщо після очищення це не порожня сторінка
+            if (!string.IsNullOrWhiteSpace(cleanText))
+            {
+                fullBookText.AppendLine(cleanText); // додавання тексту цього розділу до загального
+                fullBookText.AppendLine(); // додавання відступів між рядками
+            }
+        }
+
+        return fullBookText.ToString();
     }
 
-    var mainBody = fb2Document.Bodies?.FirstOrDefault(); // перший елемент в fb2 це сам текст
-    
-    if (mainBody != null)
+    private static string ReadFb2Book(Stream fileStream)
     {
-        return mainBody.ToString();
+        var fb2Document = new Fb2Document();
+
+        fb2Document.Load(fileStream); // завантаження stream в контейнер
+
+        if (fb2Document.Title != null)
+        {
+            // в fb2 заголовок може містити форматування, тому потрібно перетворити все в чистий рядок
+            var titleText = fb2Document.Title.ToString() ?? "The title is not specified";
+        }
+
+        var mainBody = fb2Document.Bodies?.FirstOrDefault(); // перший елемент в fb2 це сам текст
+
+        if (mainBody != null)
+        {
+            return mainBody.ToString();
+        }
+
+        return "No text chapters were found in the book";
     }
-    return "No text chapters were found in the book";
-}
 }
